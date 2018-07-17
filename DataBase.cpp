81e65de359f10c3ed28b6a1a0988864d5e8fe45a
Justyna <justyna.walkowiak6969@gmail.com>
2018-07-17 13:24:50 +0200

    bla bla

--- DataBase.cpp
+++ DataBase.cpp
@@ -1,13 +1,27 @@
 #include "DataBase.hpp"
 
+
+
 void DataBase::addNewStudent(Student s)
 {
     studentBase.push_back(s);
 }
 
 void DataBase::sortStudentsByIndex()
-{
-
+{  
+    for (int i = 1; i < studentBase.size(); ++i)
+    {
+        for (int j = studentBase.size()-1; j >= 1; j--)
+        {
+            if (studentBase.at(j).getIndex() < studentBase.at(j-1).getIndex())
+            {
+                Student buff;
+                buff = studentBase.at(j-1);
+                studentBase.at(j-1) = studentBase.at(j);
+                studentBase.at(j) = buff;
+            }
+        }
+    }
 }
 
 void DataBase::removeStudentByIndex(uint64_t idx)
