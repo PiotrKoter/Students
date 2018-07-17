368a1587ef0904b3a3edef5fd7eab81e75464028
Justyna <justyna.walkowiak6969@gmail.com>
2018-07-17 11:04:58 +0200

    crated DataBase files and fixed formatting in Student files

--- Student.hpp
+++ Student.hpp
@@ -4,20 +4,20 @@
 
 class Student
 {
-   std::string name;
-   std::string surname;
-   uint64_t index;
+    std::string name;
+    std::string surname;
+    uint64_t index;
 
 public:
-   Student();
-   ~Student();
+    Student();
+    ~Student();
 
-   void setName(std::string);
-   void setSurname(std::string);
-   void setIndex(uint64_t);
+    void setName(std::string);
+    void setSurname(std::string);
+    void setIndex(uint64_t);
 
-   std::string getName();
-   std::string getSurname();
-   uint64_t getIndex();
+    std::string getName();
+    std::string getSurname();
+    uint64_t getIndex();
 
 };
