9ec31fd1889c9caaed10091924731823aaddc7fc
marcinskalecki <marcinskalecki@hotmail.com>
2018-07-21 23:07:56 +0200

    added new class Employee

---
+++ Employee.cpp
@@ -0,0 +1,20 @@
+#include "Employee.hpp"
+
+Employee::Employee(std::string n, std::string s, float sa) :
+	name(n), surname(s), salary(sa) {}
+
+Employee::Employee() {}
+
+Employee::~Employee() {}
+
+void Employee::setName(std::string n) { name = n; }
+
+void Employee::setSurname(std::string s) { surname = s; }
+
+void Employee::setSalary(float sa) {}
+
+std::string Employee::getName() const { return name; }
+
+std::string Employee::getSurname() const { return surname; }
+
+float Employee::getSalary() const { return salary; }
---
+++ Employee.hpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+
+
+class Employee
+{
+	std::string name;
+	std::string surname;
+	float salary;
+
+public:
+	Employee(std::string, std::string, float);
+	Employee();
+	~Employee();
+
+	void setName(std::string);
+	void setSurname(std::string);
+	void setSalary(float);
+
+	std::string getName() const;
+	std::string getSurname() const;
+	float getSalary() const;
+	
+
+};
+
